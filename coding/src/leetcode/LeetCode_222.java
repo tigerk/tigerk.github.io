7f7f9d87dc52df88fb429aabda0800de3f6a6d69
金无邪 <kimhwawoon@gmail.com>
2023-08-25 12:31:54 +0800

    update codes

--- coding/src/leetcode/LeetCode_222.java
+++ coding/src/leetcode/LeetCode_222.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 /**
  * 222. 完全二叉树的节点个数
  * https://leetcode.cn/problems/count-complete-tree-nodes/description/
- *
+ * <p>
  * 给你一棵 完全二叉树 的根节点 root ，求出该树的节点个数。
  * 完全二叉树 的定义如下：在完全二叉树中，除了最底层节点可能没填满外，其余每层节点数都达到最大值，并且最下面一层的节点都集中在该层最左边的若干位置。
  * 若最底层为第 h 层，则该层包含 1~ 2h 个节点。
@@ -20,24 +20,36 @@ public class LeetCode_222 {
             return 0;
         }
 
-        int count = 0;
+        return traverse(root);
+    }
 
-        Queue<TreeNode> capacity = new LinkedList<>();
-        capacity.add(root);
-        while (!capacity.isEmpty()) {
-            int size = capacity.size();
-            count = count + size;
-            for (int i = 0; i < size; i++) {
-                TreeNode cur = capacity.poll();
-                if (cur != null && cur.left != null) {
-                    capacity.add(cur.left);
-                }
-                if (cur != null && cur.right != null) {
-                    capacity.add(cur.right);
-                }
-            }
+    public int traverse(TreeNode root) {
+        if (root == null) {
+            return 0;
         }
 
-        return count;
+        int left = 0;
+        TreeNode leftNode = root.left;
+        // 求左子树深度
+        while (leftNode != null) {
+            left++;
+            leftNode = leftNode.left;
+        }
+
+        int right = 0;
+        TreeNode rightNode = root.right;
+        // 求右子树深度
+        while (rightNode != null) {
+            right++;
+            rightNode = rightNode.right;
+        }
+        // 左右深度相等，说明是完全满二叉树
+        if (left == right) {
+            // 使用幂运算，2^n - 1 来计算节点个数
+            return (int) Math.pow(2, left + 1) - 1;
+        }
+
+        // 如果不是满的，则继续遍历左右节点。
+        return traverse(root.left) + traverse(root.right) + 1;
     }
 }
