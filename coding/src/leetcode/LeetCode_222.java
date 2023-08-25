230e3c2152becef64b290f53a4c161c39bf75057
金无邪 <kimhwawoon@gmail.com>
2023-08-25 12:40:10 +0800

    update codes

--- coding/src/leetcode/LeetCode_222.java
+++ coding/src/leetcode/LeetCode_222.java
@@ -2,9 +2,6 @@ package leetcode;
 
 import leetcode.dependency.TreeNode;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * 222. 完全二叉树的节点个数
  * https://leetcode.cn/problems/count-complete-tree-nodes/description/
